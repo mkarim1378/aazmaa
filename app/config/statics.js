8f9f3fa569605eef79f517be2771f22b0a4347b4
Hadi mardanian <mardanianhadi@gmail.com>
2022-11-27 22:19:37 +0330

    refactor: Change mongo hostname in mongoose

--- app/config/statics.js
+++ app/config/statics.js
@@ -1,7 +1,7 @@
 const path = require('path');
 module.exports = {
     botToken: '1475208126:AAEl8YREFaS-4w2AsNscDdFUq7ZN2XbhOEo',
-    connectionString: 'mongodb://localhost/aazmaa',
+    connectionString: 'mongodb://aazmaa-mongo/aazmaa',
     port: 4000,
     
     views: {
